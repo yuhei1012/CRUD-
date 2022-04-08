ee91f55bf00b27b68c51a32a1a83f5bc74e845e3
yuhei1012 <ychachinana@gmail.com>
2022-04-08 12:03:13 +0000

    “ブランチの確認”

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root 'tweets#index'  # 追加
+
+  get 'tweets/new'
+  get 'tweets/index'
+  get 'tweets/show'
+  get 'tweets/create'
+  get 'users/index'
+  get 'users/show'
+  
+  devise_for :users
+  
+  resources :tweets  # 追加
+  resources :users  # 追加
+
 end
