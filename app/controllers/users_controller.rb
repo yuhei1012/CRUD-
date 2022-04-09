f6da2473c2be6169f81b33a1de4f3a78603cbb38
yuhei1012 <ychachinana@gmail.com>
2022-04-09 07:03:27 +0000

    ユーザーがファボしたツイートの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,11 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-# ===============追加==============
     @tweets = @user.tweets
-# ================================
+    @favorite_tweets = @user.favorite_tweets # 追加
   end
 end
