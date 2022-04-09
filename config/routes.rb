b55fa8278eb363fcf7b646d614eab3a8913b1b4e
yuhei1012 <ychachinana@gmail.com>
2022-04-09 06:01:14 +0000

    ネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
-  
-  get 'favorites/create'
-  get 'favorites/destroy'
-  root 'tweets#index'  # 追加
-
-  get 'tweets/new'
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/create'
-  get 'users/index'
-  get 'users/show'
-  
+  #==================削除orコメントアウト================
+  # get 'favorites/create'
+  # get 'favorites/destroy' 
+  #=====================================
+  root 'tweets#index'
   devise_for :users
-  
-  resources :tweets  # 追加
-  resources :users  # 追加
 
+  # ================ここをネスト(入れ子)した形に変更
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+  end
+  #======================================
+
+  resources :users 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
