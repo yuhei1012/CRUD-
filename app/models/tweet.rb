7eb61c347ed6e927ec9eb1850f5f77ecdaebb1a0
yuhei1012 <ychachinana@gmail.com>
2022-04-09 06:32:29 +0000

    判定メソッド

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,9 @@
 class Tweet < ApplicationRecord
     belongs_to :user
-    has_many :favorites  # 追加
+    has_many :favorites
+
+# 追加
+    def favorited_by?(user)
+        favorites.where(user_id: user.id).exists?
+    end
 end
