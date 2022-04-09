c58b2dfac37d2cd3613da11bb67fe9a96e9ba33a
yuhei1012 <ychachinana@gmail.com>
2022-04-09 07:21:28 +0000

    中間テーブルRelationshipモデルの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
+
   #==================削除orコメントアウト================
-  # get 'favorites/create'
-  # get 'favorites/destroy' 
+  # get 'relationships/create'
+  # get 'relationships/destroy'
   #=====================================
-  root 'tweets#index'
+  root 'tweets#index' 
+
   devise_for :users
 
-  # ================ここをネスト(入れ子)した形に変更
   resources :tweets do
     resource :favorites, only: [:create, :destroy]
   end
-  #======================================
 
-  resources :users 
+  # ================ここをネスト(入れ子)した形に変更
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member # 追加
+    get :followers, on: :member # 追加
+  end
+  #======================================
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
